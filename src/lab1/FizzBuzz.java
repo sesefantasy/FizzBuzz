2dfe2cdd016760a6f9e27be8ab22a16a10d685d9
xiayu <fantieshu@gmail.com>
2025-09-10 16:38:07 -0400

    practice task 3.3

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -5,9 +5,9 @@ package lab1;
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void doFizzBuzz(int j) {
         int i = 1;
-        while (i < 100) {
+        while (i < j) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
